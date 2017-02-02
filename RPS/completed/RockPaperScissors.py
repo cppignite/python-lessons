a0b0565756668c33cefddc241dcc5d831032bf00
zkysar <zach.kysar@gmail.com>
2017-02-01 18:57:59 -0800

    Add more simplifications to the final program

--- RPS/completed/RockPaperScissors.py
+++ RPS/completed/RockPaperScissors.py
@@ -1,50 +1,22 @@
-import sys
-import os
+user1_answer = input("Player 1, do you want to choose rock, paper or scissors?")
+user2_answer = input("Player 2, do you want to choose rock, paper or scissors?")
 
-user1_score = 0
-user2_score = 0
-
-turn_num = 0
-
-
-def compare(u1, u2):
-    if u1 == u2:
-        return("It's a tie! The scores weren't changed.")
-    elif u1 == 'rock':
-        if u2 == 'scissors':
-            user1_score += 1
-            return("Rock wins this round!")
-        else:
-            user2_score += 1
-            return("Paper wins this round!")
-    elif u1 == 'scissors':
-        if u2 == 'paper':
-            user1_score += 1
-            return("Scissors win this round!")
-        else:
-            user2_score += 1
-            return("Rock wins this round!")
-    elif u1 == 'paper':
-        if u2 == 'rock':
-            user1_score += 1
-            return("Paper wins this round!")
-        else:
-            user2_score += 1
-            return("Scissors win this round!")
+if user1_answer == user2_answer:
+    print("It's a tie! The scores weren't changed.")
+elif user1_answer == 'rock':
+    if user2_answer == 'scissors':
+        print("Player 1 wins this round!")
     else:
-        return("Invalid input! You have not entered rock, paper or scissors, try again.")
-        sys.exit()
-
-for turn in range(1,4):
-    os.system('clear')
-    user1_answer = input("Player 1, do you want to choose rock, paper or scissors?")
-    os.system('clear')
-    user2_answer = input("Player 2, do you want to choose rock, paper or scissors?")
-    print(compare(user1_answer, user2_answer))
-
-if user1_score > user2_score:
-    print ("Congratulations Player 1, you won this game!")
-elif user2_score > user1_score:
-    print ("Congratulations Player 2, you won this game!")
+        print("Player 2 wins this round!")
+elif user1_answer == 'scissors':
+    if user2_answer == 'paper':
+        print("Player 1 win this round!")
+    else:
+        print("Player 2 wins this round!")
+elif user1_answer == 'paper':
+    if user2_answer == 'rock':
+        print("Player 1 wins this round!")
+    else:
+        print("Player 2 win this round!")
 else:
-    print ("Wow you two tied this game!")
+    print("Invalid input! You have not entered rock, paper or scissors, try again.")
